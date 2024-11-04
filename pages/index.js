1b1785627f13501f72d9b5878b58f703eab62618
jcosilva <144964918+jcosilva@users.noreply.github.com>
2024-11-04 15:56:16 +0000

    Alteração frase para Vanessa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Van, eu gostaria muito que você fosse a primeira pessoa que visse um pedacinho desta nova história que estamos escrevendo... muito obrigado pela força! Te amo mais que demais! Mil Bjs! Seu... Ju</h1>
+    return <h1>Van, eu gostaria muito que você fosse a primeira pessoa que visse um pedacinho desta nova história que estamos escrevendo... muito obrigado pela força! Te amo mais que demais! Mil Bjs! Seu... Ju ❤️❤️❤️</h1>
 }
 
 export default Home;
